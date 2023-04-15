36891c11354fb40ecaf814a9fc60005af6e5471a
Pratik_Umap <103456799+pratik-umap@users.noreply.github.com>
2023-04-15 12:50:41 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cookieParser = require("cookie-parser");
+const cors = require('cors');
 
 if (process.env.NODE_END !== "production") {
   require("dotenv").config({ path: "backend/config/config.env" });
@@ -10,6 +11,7 @@ if (process.env.NODE_END !== "production") {
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser());
+app.use(cors());
 
 
 // importing routes
