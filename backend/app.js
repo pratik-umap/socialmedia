1d7d7915780f5d4d2d01f6fadf207d3918ef6492
Pratik_Umap <103456799+pratik-umap@users.noreply.github.com>
2023-04-16 11:49:59 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -15,10 +15,10 @@ app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser());
 
 // static file
-app.use(express.static(path.join(__dirname,"/frontend/build")))
+app.use(express.static(path.join(__dirname,"/build")))
 
 app.get('*',function(req,res) {
-   res.sendFile(path.join(__dirname,"/frontend/build/index.html"))
+   res.sendFile(path.join(__dirname,"/build/index.html"))
 })
 
 // importing routes
