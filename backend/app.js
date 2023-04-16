7ef090113685170cbdc9ab68bc105c263593c527
Pratik_Umap <103456799+pratik-umap@users.noreply.github.com>
2023-04-16 11:58:39 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,7 @@ app.use(cookieParser());
 // static file
 app.use(express.static(path.join(__dirname,"/build")))
 
-app.get('*',function(req,res) {
+app.get('/',function(req,res) {
    res.sendFile(path.join(__dirname,"/build/index.html"))
 })
 
